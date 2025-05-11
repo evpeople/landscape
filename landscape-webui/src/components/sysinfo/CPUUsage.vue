5deedebc16afaf7f71f38651e0add6696c6d7db5
evpeople <hangcaihui@gmail.com>
2025-05-11 16:51:33 +0800

    style: Format i18n files and CPUUsage.vue
    Fixes minor formatting issues in i18n and CPUUsage.vue.

--- landscape-webui/src/components/sysinfo/CPUUsage.vue
+++ landscape-webui/src/components/sysinfo/CPUUsage.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import {computed} from "vue";
-import {ExhibitType} from "@/lib/sys";
+import { computed } from "vue";
+import { ExhibitType } from "@/lib/sys";
 
 import SourceProgress from "@/components/SouceProgress.vue";
-import {useSysInfo} from "@/stores/systeminfo";
-import {useI18n} from "vue-i18n";
+import { useSysInfo } from "@/stores/systeminfo";
+import { useI18n } from "vue-i18n";
 
-const {t} = useI18n({useScope: "global"});
+const { t } = useI18n({ useScope: "global" });
 
 let sysinfo = useSysInfo();
 
@@ -29,107 +29,116 @@ const load_avg = computed(() => {
     <n-flex style="flex: 1" vertical justify="space-between">
       <n-flex vertical justify="space-between">
         <n-flex justify="space-between">
-          <n-flex>{{ t('total_cpu_usage') }}</n-flex>
+          <n-flex>{{ t("total_cpu_usage") }}</n-flex>
           <n-flex>
             {{ sysinfo.router_status.global_cpu_info.toFixed(1) }} %
           </n-flex>
         </n-flex>
 
         <n-flex justify="space-between">
-          <n-flex>{{ t('average_load') }}</n-flex>
+          <n-flex>{{ t("average_load") }}</n-flex>
           <n-flex>
             {{ `${load_avg.one} / ${load_avg.five} / ${load_avg.fifteen}` }}
           </n-flex>
         </n-flex>
       </n-flex>
 
-      <n-flex v-if="sysinfo.router_status.cpus.length <= 4" style="overflow: hidden;">
+      <n-flex
+        v-if="sysinfo.router_status.cpus.length <= 4"
+        style="overflow: hidden"
+      >
         <n-scrollbar>
           <n-flex>
             <n-popover
-                v-for="each_cpu of sysinfo.router_status.cpus"
-                :key="each_cpu.name"
-                trigger="hover"
+              v-for="each_cpu of sysinfo.router_status.cpus"
+              :key="each_cpu.name"
+              trigger="hover"
             >
               <template #trigger>
                 <SourceProgress
-                    :exhibit_type="ExhibitType.Line"
-                    :value="each_cpu.usage / 100"
+                  :exhibit_type="ExhibitType.Line"
+                  :value="each_cpu.usage / 100"
                 />
               </template>
-              <span>{{ each_cpu.name }}: {{ each_cpu.brand }} @ {{ each_cpu.frequency }}</span>
+              <span
+                >{{ each_cpu.name }}: {{ each_cpu.brand }} @
+                {{ each_cpu.frequency }}</span
+              >
             </n-popover>
           </n-flex>
         </n-scrollbar>
       </n-flex>
 
       <n-grid
-          v-else-if="sysinfo.router_status.cpus.length <= 8"
-          :cols="2"
-          :x-gap="12"
-          :y-gap="12"
+        v-else-if="sysinfo.router_status.cpus.length <= 8"
+        :cols="2"
+        :x-gap="12"
+        :y-gap="12"
       >
         <n-gi
-            v-for="each_cpu of sysinfo.router_status.cpus"
-            :key="each_cpu.name"
+          v-for="each_cpu of sysinfo.router_status.cpus"
+          :key="each_cpu.name"
         >
           <n-popover trigger="hover">
             <template #trigger>
               <SourceProgress
-                  :exhibit_type="ExhibitType.Line"
-                  :value="each_cpu.usage / 100"
+                :exhibit_type="ExhibitType.Line"
+                :value="each_cpu.usage / 100"
               />
             </template>
-            <span>{{ each_cpu.name }}: {{ each_cpu.brand }} @ {{ each_cpu.frequency }}</span>
+            <span
+              >{{ each_cpu.name }}: {{ each_cpu.brand }} @
+              {{ each_cpu.frequency }}</span
+            >
           </n-popover>
         </n-gi>
       </n-grid>
 
       <n-grid
-          v-else-if="sysinfo.router_status.cpus.length <= 12"
-          :cols="3"
-          :x-gap="12"
-          :y-gap="12"
+        v-else-if="sysinfo.router_status.cpus.length <= 12"
+        :cols="3"
+        :x-gap="12"
+        :y-gap="12"
       >
         <n-gi
-            v-for="each_cpu of sysinfo.router_status.cpus"
-            :key="each_cpu.name"
+          v-for="each_cpu of sysinfo.router_status.cpus"
+          :key="each_cpu.name"
         >
           <n-popover trigger="hover">
             <template #trigger>
               <SourceProgress
-                  :exhibit_type="ExhibitType.Line"
-                  :value="each_cpu.usage / 100"
+                :exhibit_type="ExhibitType.Line"
+                :value="each_cpu.usage / 100"
               />
             </template>
-            <span>{{ each_cpu.name }}: {{ each_cpu.brand }} @ {{ each_cpu.frequency }}</span>
+            <span
+              >{{ each_cpu.name }}: {{ each_cpu.brand }} @
+              {{ each_cpu.frequency }}</span
+            >
           </n-popover>
         </n-gi>
       </n-grid>
 
       <!-- 当 CPU > 12 时 -->
-      <n-grid
-          v-else
-          :cols="4"
-          :x-gap="12"
-          :y-gap="12"
-      >
+      <n-grid v-else :cols="4" :x-gap="12" :y-gap="12">
         <n-gi
-            v-for="each_cpu of sysinfo.router_status.cpus"
-            :key="each_cpu.name"
+          v-for="each_cpu of sysinfo.router_status.cpus"
+          :key="each_cpu.name"
         >
           <n-popover trigger="hover">
             <template #trigger>
               <SourceProgress
-                  :exhibit_type="ExhibitType.Line"
-                  :value="each_cpu.usage / 100"
+                :exhibit_type="ExhibitType.Line"
+                :value="each_cpu.usage / 100"
               />
             </template>
-            <span>{{ each_cpu.name }}: {{ each_cpu.brand }} @ {{ each_cpu.frequency }}</span>
+            <span
+              >{{ each_cpu.name }}: {{ each_cpu.brand }} @
+              {{ each_cpu.frequency }}</span
+            >
           </n-popover>
         </n-gi>
       </n-grid>
-
     </n-flex>
     <!-- {{ sysinfo.cpus }} -->
   </n-card>
--- landscape-webui/src/i18n/en/main.ts
+++ landscape-webui/src/i18n/en/main.ts
@@ -1,6 +1,6 @@
 export default {
-    docker_divider: "Docker Containers",
-    topology_divider: "Network topology",
-    total_cpu_usage: "Total Cpu Usage:",
-    average_load: "Average Load:"
+  docker_divider: "Docker Containers",
+  topology_divider: "Network topology",
+  total_cpu_usage: "Total Cpu Usage:",
+  average_load: "Average Load:",
 };
